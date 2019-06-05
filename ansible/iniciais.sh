2ba4423319ffcfb71d99c5985fe3fdaf1f44fbbd
Your Name <you@example.com>
2019-06-05 17:56:41 -0400

    ajustes

--- ansible/iniciais.sh
+++ ansible/iniciais.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-yes | ssh-keygen -N "" -f ~/.ssh/id_rsa
+####  yes | ssh-keygen -N "" -f ~/.ssh/id_rsa
+
+ansible all -m ping 
 
 ansible all -m ping -k 
 
 ansible all -m authorized_key -a 'user=root  key={{ lookup("file", "/root/.ssh/id_rsa.pub") }}'
 
 ansible all  -m shell  -a "yum install -y net-tools"
+
+ansible all  -m shell  -a "ifconfig"
